75d6aa35d5fb0263270e669d00d8189019e4ab28
thivav <thivaharan28@gmail.com>
2024-11-21 10:44:50 +0530

    new images

--- scripts/archive_imgs.sh
+++ scripts/archive_imgs.sh
@@ -3,13 +3,16 @@
 # define variables
 DIRECTORY_x100="../data/processed_data/v1.0/imgs/x100_small"
 DIRECTORY_x300="../data/processed_data/v1.0/imgs/x300_medium"
+SOURCE="../data/processed_data/v1.0/imgs"
+
 DESTINATION="../data/processed_data/v1.0/archive/"
 ARCHIVE="imgs.tar.gz"
 PART_PREFIX="imgs_part_"
 PART_SIZE="20M"
 
 # create tar.gz archive
-tar -zcvf $ARCHIVE $DIRECTORY_x100 $DIRECTORY_x300
+# tar -zcvf $ARCHIVE $DIRECTORY_x100 $DIRECTORY_x300
+tar -zcvf $ARCHIVE $SOURCE
 
 # split the archive into smaller parts
 split -b $PART_SIZE $ARCHIVE $PART_PREFIX
